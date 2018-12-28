74eb2a6c72bdca56761e43eb07d05ca803aa95e0
Brady Snuggs <dronebug360@gmail.com>
2018-12-28 13:32:57 -0700

    Started buildining interior of Art Gallery.  Needs texture on floor, and collision parameters on walls.

--- src/components/LandingPage/register.js
+++ src/components/LandingPage/register.js
@@ -1,10 +1,6 @@
 import React from 'react'
 import stylus from 'stylus-in-react'
 
-const Button = stylus`
-button
-    width 100px
-`
 
 const register = (props) => {
     let {handleChange} = props
@@ -16,7 +12,7 @@ const register = (props) => {
             <input placeholder="username" name="username" onChange={handleChange}></input>
             <input placeholder="password" type="password" name="password" onChange={handleChange}></input>
             <input placeholder="email" name="email" onChange={handleChange}></input>
-            <Button> </Button>
+            
         </section>
         </>
     )
--- src/views/ArtGallery.js
+++ src/views/ArtGallery.js
@@ -1,6 +1,11 @@
-import React, {Component} from 'react';
-import 'aframe';
-import {Entity, Scene} from 'aframe-react'
+import React, {Component} from 'react'
+import 'aframe'
+import {Entity, Scene, Assets} from 'aframe-react'
+import 'aframe-particle-system-component'
+import 'aframe-room-component'
+import 'aframe-physics-system'
+
+
 
 class ArtGallery extends Component{
     constructor(){
@@ -16,10 +21,25 @@ render(){
     return(
         <>
             <Scene>
-                <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
-
-                    
-                <Entity primitive='a-sky' src="../assets/sky.jpeg"/>
+                    <rw-room position="-2 0 -2" material="color:#866">
+                    <rw-floor material="color:#000"></rw-floor>
+                    <rw-ceiling></rw-ceiling>
+                    <rw-wall position="10 0 0" height="6m"></rw-wall>
+                    <rw-wall position="10 0 10" height="6m"></rw-wall>
+                    <rw-wall position="0 0 10" height="6m"></rw-wall>
+                    <rw-wall position="0 0 0" height="6m">
+                        <rw-doorhole id="holeA"></rw-doorhole>
+                        <rw-doorlink from="#holeA" to="#holeB" position="2.5 0 0"></rw-doorlink>
+                    </rw-wall>
+                </rw-room>
+                <rw-room position="0 0 -3">
+                    <rw-wall position=" 1 0 -1" material="color:#787"></rw-wall>
+                    <rw-wall position=" 1 0  1" material="color:#877">
+                        <rw-doorhole id="holeB"></rw-doorhole>
+                    </rw-wall>
+                    <rw-wall position="-1 0  1" material="color:#878"></rw-wall>
+                    <rw-wall position="-1 0 -1" material="color:#778"></rw-wall>
+                </rw-room>  
             </Scene>
         </>
     )
