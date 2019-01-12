a2673d822fdc9482356246bcd5e66a950aabb056
jakerowla <jakenrowland@gmail.com>
2019-01-11 17:49:58 -0700

    Basic HomePage layout

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -11,12 +11,23 @@ class LandingPage extends Component {
             <div className='landing'>
                 <header className='landing-header'>
                     <div className='landing-header_name'>
-                    VR <span className='lighttext'>ART GALLERY</span>
+                        VR <span className='lighttext'>ART GALLERY</span>
                     </div>
                     <div className='landing-header_links'>
-                        <span>LOGIN</span><span> | </span><span>REGISTER</span>
+                        <span>LOGIN</span>
+                        <span>REGISTER</span>
+                        <div className='landing-header_search'>
+                            <input type='text' placeholder='Search Galleries'></input>
+                        </div>
                     </div>
                 </header>
+                <main className='landing-main center'>
+                    <div className='landing-main_body'>
+                        <h1>Walk around and see cool shit in VR</h1>
+                        <h3>Make your own or browse other galleries</h3>
+                        <div className='center'>View Galleries</div>
+                    </div>
+                </main>
             </div>
         )
     }
