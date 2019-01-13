38993ba70f2a3e715ef0d1eee5a6f902fcc5ed0b
jakerowla <jakenrowland@gmail.com>
2019-01-12 19:03:53 -0700

    Added button that scrolls to the top of the page

--- src/views/LandingPage.js
+++ src/views/LandingPage.js
@@ -1,10 +1,72 @@
 import React, { Component } from 'react'
 import '../Styles/LandingPage.css'
 import VRVideo from '../assets/video/VR-Trailer.mp4'
+import { homedir } from 'os';
 
 class LandingPage extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            mobileToggle: false
+        }
+    }
+
+    componentDidMount() {
+        window.addEventListener('scroll', this.handleScroll)
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('scroll', this.handleScroll)
+    }
+
+    debounce = (func, wait, immediate) => {
+        let timeout
+        return function () {
+            let context = this, args = arguments
+            let later = function () {
+                timeout = null
+                if (!immediate) func.apply(context, args)
+            }
+            let callNow = immediate && !timeout
+            clearTimeout(timeout)
+            timeout = setTimeout(later, wait)
+            if (callNow) func.apply(context, args)
+        }
+    }
+
+    handleMobile = () => {
+
+    }
+
+    handleScroll = this.debounce(() => {
+        const scroll = window.scrollY
+        const back = document.querySelector('.landing-back')
+        if (scroll >= 650) {
+            back.classList.add('animate-fadeIn')
+            setTimeout(() => {
+                back.style.visibility = 'visible'
+            }, 1000);
+        }
+        else {
+            back.classList.remove('animate-fadeIn')
+            back.classList.add('animate-fadeOut')
+            setTimeout(() => {
+                back.style.visibility = 'hidden'
+                back.classList.remove('animate-fadeOut')
+            }, 1000);
+        }
+    }, 150)
+
+    handleClick = (target) => {
+        if (target === 'gallery') {
+            const main = document.querySelector('.landing-main')
+            main.scrollIntoView()
+        }
+        else if (target === 'top') {
+            console.log('helo')
+            const top = document.querySelector('.home')
+            top.scrollIntoView()
+        }
     }
 
     render() {
@@ -18,16 +80,13 @@ class LandingPage extends Component {
                         <div className='landing-header_links'>
                             <span>LOGIN</span>
                             <span>REGISTER</span>
-                            <div className='landing-header_search center'>
-                                <input type='text' placeholder='SEARCH GALLERIES'></input>
-                            </div>
                         </div>
                     </header>
                     <div className='landing-welcome'>
                         <div className='landing-welcome_body'>
                             <h1>Walk around and see cool shit in VR</h1>
                             <h3>Make your own or browse other galleries</h3>
-                            <div className='center'>VIEW GALLERIES</div>
+                            <div className='center' onClick={() => this.handleClick('gallery')}>VIEW GALLERIES</div>
                             <div className='video-container center'>
                                 <video controls src={VRVideo} alt="trailer of VR-Art-Gallery"></video>
                             </div>
@@ -35,7 +94,10 @@ class LandingPage extends Component {
                     </div>
                 </div>
                 <main className='landing-main'>
-
+                    
+                    <div className='landing-back center' onClick={() => this.handleClick('top')}>
+                        <i className="fas fa-arrow-up"></i>
+                    </div>
                 </main>
             </div>
         )
