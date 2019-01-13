fec54a106406910002dda0d679288aa384f98ac2
jakerowla <jakenrowland@gmail.com>
2019-01-13 13:55:18 -0700

    Cleaned up Landing styling and added placeholder picture to gallery cards

--- server/server.js
+++ server/server.js
@@ -1,17 +1,15 @@
 require('dotenv').config()
     const express = require('express'),
     app = express(),
-    bodyParser = require('body-parser'),
-    ctrl = require('./controller')
+    ctrl = require('./controller'),
+    { SERVER_PORT } = process.env
 
-    let {SERVER_PORT} = process.env
-
-    app.use(bodyParser.json())
+    app.use(express.static( `${__dirname}/../build` ));
+    app.use(express.json())
 
     app.get('/api/register', ctrl.registerUser)
-    
     app.get('/api/getImages/', ctrl.getImages)
 
 
     PORT = SERVER_PORT || 4500
-    app.listen(PORT, ()=> console.log(`Someone is looking at Art on ${PORT}`))
+    app.listen(PORT, () => console.log(`Someone is looking at Art on ${PORT}`))
