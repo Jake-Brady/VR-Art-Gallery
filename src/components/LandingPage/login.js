df4685e1283ceb329ba839936a4a752a4ae680a2
jakerowla <jakenrowland@gmail.com>
2019-01-14 15:36:07 -0700

    Styled Login Component

--- src/components/LandingPage/login.js
+++ src/components/LandingPage/login.js
@@ -1,58 +1,67 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
-import '../Styles/login.css'
-import {withRouter} from 'react-router-dom'
+import '../../Styles/Components/login.css'
+import logo from '../../Styles/Media/Icon.png'
+import { withRouter } from 'react-router-dom'
 
-class Login extends Component{
-    constructor(){
+class Login extends Component {
+    constructor() {
         super()
-            this.state={
-                username: '',
-                password: '',
-                loginMsg: ''
-            }
-            this.handleChange = this.handleChange.bind(this)
-            this.login = this.login.bind(this)
+        this.state = {
+            username: '',
+            password: '',
+            loginMsg: ''
+        }
+        this.handleChange = this.handleChange.bind(this)
+        this.login = this.login.bind(this)
     }
 
-handleChange(e){
-this.setState({
-    [e.target.name]: e.target.value
-})
-}
-
-login(){
-let {username, password} = this.state
-axios.post(`/api/login`, {username, password}).then(res => {
-    //If username does not exist, inform user
-    if (res.data === 'Wrong Username'){
-        this.setState({loginMsg: 'This username does not exist.'}, () => {
-            alert('This username does not exist.')
+    handleChange(e) {
+        this.setState({
+            [e.target.name]: e.target.value
         })
-    //If password does not match username, inform user
-    } else if (res.data === 'Wrong Password'){
-        this.setState({loginMsg: 'Incorrect password.'}, () => {
-            alert('Password is incorrect.')
-        })
-    //If all above is false, then username and password must match and user will be redirect to lobby view.
-    } else {
-        let {username} = res.data
-        console.log(username)
-        this.props.history.push(`/lobby/${username}`)
-    }  
-})
-} 
+    }
 
-render(){
-    return(
-        <section id="login-section">
-            <p>login component</p>
-            <input name="username" placeholder="username" onChange={this.handleChange}></input>
-            <input name="password" placeholder="password" type="password" onChange={this.handleChange}></input>
-            <button onClick={this.login}>Login</button>
-        </section>
-    )
-}
+    login() {
+        let { username, password } = this.state
+        axios.post(`/api/login`, { username, password }).then(res => {
+            //If username does not exist, inform user
+            if (res.data === 'Wrong Username') {
+                this.setState({ loginMsg: 'This username does not exist.' }, () => {
+                    alert('This username does not exist.')
+                })
+                //If password does not match username, inform user
+            } else if (res.data === 'Wrong Password') {
+                this.setState({ loginMsg: 'Incorrect password.' }, () => {
+                    alert('Password is incorrect.')
+                })
+                //If all above is false, then username and password must match and user will be redirect to lobby view.
+            } else {
+                let { username } = res.data
+                this.props.history.push(`/lobby/${username}`)
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className="login-container">
+            <div className='login-header'>
+                <img src={logo} alt='VR Logo' />
+                <span>VR<span className='lighttext'>ART GALLERY</span></span>
+            </div>
+                <div className='login-content'>
+                    {/* <button onClick={this.login}>Login</button> */}
+                    <h1>Log In</h1>
+                    <input name="username" placeholder="Username" onChange={this.handleChange}></input>
+                    <input name="password" placeholder="Password" type="password" onChange={this.handleChange}></input>
+                    <div>Log In</div>
+                    <h2>Forgot Password?</h2>
+                </div>
+                <h1>Don't have an account? <span className='login-sign'>Sign Up.</span></h1>
+            </div>
+        )
+    }
 }
 
 export default withRouter(Login)
