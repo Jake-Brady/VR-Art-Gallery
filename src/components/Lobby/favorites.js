a08ec1740bca111d6b57d09656f960c38700f567
jakerowla <jakenrowland@gmail.com>
2019-01-16 15:15:08 -0700

    Favorites and Galleries basic grid setup

--- src/components/Lobby/favorites.js
+++ src/components/Lobby/favorites.js
@@ -2,26 +2,25 @@ import React from 'react'
 import '../Styles/favorites.css'
 
 const Favorites = (props) => {
-let {id, image, views, shares, favoriteNum, visitGallery, galleryName, author } = props
-console.log(id)
-return(
-<div key={id} className="gallery-container">
-   <img src={image} alt="gallery thumbnail" className="gallery-thumbnail" />
-   <div className='gallery-text'>
-      <h1 className='gallery-title'>{galleryName}</h1>
-      <h3 className='gallery-author'>BY: {author}</h3>
-      <div className='gallery-stats'>
-         <i className="fas fa-eye stat"></i>
-         <span>{views}</span>
-         <i className="fas fa-heart stat"></i>
-         <span>{favoriteNum}</span>
-         <i className="fas fa-share stat"></i>
-         <span>{shares}</span>
-       </div>
-   </div>
-   <span className="gallery-view" onClick={() => visitGallery(galleryName,author)}>Visit Gallery</span>
-</div>
-)
+   let { id, image, views, shares, favoriteNum, visitGallery, galleryName, author } = props
+   return (
+      <div key={id} className="gallery-container">
+         <img src={image} alt="gallery thumbnail" className="gallery-thumbnail" />
+         <div className='gallery-text'>
+            <h1 className='gallery-title'>{galleryName}</h1>
+            <h3 className='gallery-author'>BY: {author}</h3>
+            <div className='gallery-stats'>
+               <i className="fas fa-eye stat"></i>
+               <span>{views}</span>
+               <i className="fas fa-heart stat"></i>
+               <span>{favoriteNum}</span>
+               <i className="fas fa-share stat"></i>
+               <span>{shares}</span>
+            </div>
+            <div className='gallery-view center' onClick={() => visitGallery(galleryName, author)}>Visit Gallery</div>
+         </div>
+      </div>
+   )
 }
 
 export default Favorites
