37ebc788d9e42fdb35d4b4aeabcb50b3115764a5
jakerowla <jakenrowland@gmail.com>
2019-01-17 18:31:08 -0700

    Removed unnecessary packages and cleaned up server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-let {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, ENVIRONMENT} = process.env
+const {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, ENVIRONMENT} = process.env
     const express = require('express'),
     session = require('express-session')({
         secret: SESSION_SECRET,
@@ -7,23 +7,10 @@ let {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, ENVIRONMENT} = process.env
         saveUninitialized: true,
     }),
     app = express(),
-    bodyParser = require('body-parser'),
     massive = require('massive'),
     ctrl = require('./controller')
-    app.use(bodyParser.json())
+    app.use(express.json())
     app.use(session)
-    
-//     app.use((req,res,next) => {
-//     if (ENVIRONMENT === 'dev') {
-//         req.app.get('db').set_data()
-//         .then(userData => {
-//             req.session.user = userData[0]
-//             next()
-//         })
-//     } else {
-//         next()
-//     }
-// })
 
     /* VR-Art-Gallery Endpoints */
     //Landing Page - Register/Login
@@ -38,9 +25,7 @@ let {SERVER_PORT, CONNECTION_STRING, SESSION_SECRET, ENVIRONMENT} = process.env
     app.get('/api/checkUser/', ctrl.checkUser)
     app.get('/api/retrieveGalleries/', ctrl.retrieveGalleries)
     app.get('/api/getFavorites/', ctrl.getFavorites)
-
     app.post('/api/logout/', ctrl.logout)
-
     app.delete('/api/deleteGallery/:id', ctrl.deleteGallery)
     
     //Art-Gallery
