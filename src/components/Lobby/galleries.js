490b5a1f612c1b01e0bc8f141e4a1a5f72096010
jakerowla <jakenrowland@gmail.com>
2019-01-21 18:11:44 -0700

    Added edit/delete popup for galleries

--- src/components/Lobby/galleries.js
+++ src/components/Lobby/galleries.js
@@ -1,29 +1,40 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Galleries = (props) => {
-    let {id, image, views, shares, favoriteNum, galleryName, author, visitGallery, editGallery, deleteGallery, isPrivate } = props
-    return(
-        <div key={id} className="gallery-container">
-            <img src={image} alt="gallery thumbnail" className="gallery-thumbnail" />
-            <div className='gallery-text'>
-                <h1 className='gallery-title'>{galleryName}</h1>
-                <h3 className='gallery-author'>BY: {author}</h3>
-                <div className='gallery-stats'>
-                {/* if isPrivate is truthy, then lock icon will appear. If false, it will be unlocked icon. */}
-                    {isPrivate ? <><i className="fas fa-lock stat"></i> <span>Private</span></> : <><i className="fas fa-unlock stat"></i> <span>Public</span> </>}
-                    <i className="fas fa-eye stat"></i>
-                    <span>{views}</span>
-                    <i className="fas fa-heart stat"></i>
-                    <span>{favoriteNum}</span>
-                    <i className="fas fa-share stat"></i>
-                    <span>{shares}</span>
+class Galleries extends Component {
+
+    toggleDrop = id => {
+        const pop = [...document.querySelectorAll('[data-pop]')],
+        index = pop.map(pop => pop.dataset.pop).findIndex(index => index == id),
+        filtered = pop.filter(pop => pop.dataset.pop != id),
+        target = pop[index]
+        target.classList.contains('gallery-pop_show') ? target.classList.remove('gallery-pop_show') : target.classList.add('gallery-pop_show')
+        filtered.map(pop => pop.classList.remove('gallery-pop_show'))
+    }
+
+    render() {
+        let { id, image, views, shares, favoriteNum, galleryName, author, visitGallery, editGallery, deleteGallery, isPrivate } = this.props
+        return (
+            <div key={id} className='gallery-container'>
+                <img src={image} alt='Card Thumbnail' className='gallery-thumbnail' />
+                <div className='gallery-text'>
+                    <h1 className='gallery-title'>{galleryName}</h1>
+                    <h3 className='gallery-author'>BY:{author}</h3>
+                    <div className='gallery-stats'>
+                        {isPrivate ? <><i className="fas fa-lock stat"></i> <span>Private</span></> : <><i className="fas fa-unlock stat"></i> <span>Public</span> </>}
+                        <i className="fas fa-eye stat"></i><span>{views}</span>
+                        <i className="fas fa-heart stat"></i><span>{favoriteNum}</span>
+                        <i className="fas fa-share stat"></i><span>{shares}</span>
+                        <i className="fas fa-ellipsis-v gallery-option" onClick={() => this.toggleDrop(id)}></i>
+                        <div className='gallery-pop' data-pop={id}>
+                            <div className='center'>EDIT</div>
+                            <div className='center'>DELETE</div>
+                        </div>
+                    </div>
+                    <div onClick={() => visitGallery(galleryName, author)} className='gallery-view center'>Visit Gallery</div>
                 </div>
             </div>
-            <span className="gallery-view" onClick={() => visitGallery(galleryName,author)}>Visit Gallery</span>
-            <span className="gallery-view" onClick={() => editGallery(id)}>Edit Gallery</span>
-            <span className="gallery-view" onClick={() => deleteGallery(id, galleryName)}>Delete Gallery</span>
-        </div>
-    )
+        )
+    }
 }
 
 export default Galleries
