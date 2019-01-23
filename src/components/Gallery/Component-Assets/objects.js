d43fd041ade93aa64c5a509b11d780f2a86c0e2f
Brady Snuggs <dronebug360@gmail.com>
2019-01-22 18:31:51 -0700

    Reduced size of obj and converted to glb file for statue of nymph.  Needs to be tested for quality before adding other models.

--- src/components/Gallery/Component-Assets/objects.js
+++ src/components/Gallery/Component-Assets/objects.js
@@ -1,4 +1,4 @@
-import nymph from '../../../assets/3d_objs/nymph.obj'
+import nymph from '../../../assets/3d_objs/gltf/newNymph.glb'
 import 'aframe'
 import {Entity} from 'aframe-react'
 import React, {Component} from 'react'
