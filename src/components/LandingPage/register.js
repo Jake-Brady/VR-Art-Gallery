17b9c702f997e72e62662ae105abb628e70c5453
jakerowla <jakenrowland@gmail.com>
2019-01-26 22:47:52 -0700

    Register alerts have been replaced with error text

--- src/components/LandingPage/register.js
+++ src/components/LandingPage/register.js
@@ -35,28 +35,100 @@ class Register extends Component {
     }
 
     highLightFields = () => {
-        const inputs = [...document.querySelectorAll('#register-inputs > input')]
-        inputs.map(input => input.style.borderColor = 'rgba(0, 0, 0, 0.185)')
+        const inputs = [...document.querySelectorAll('#register-inputs > input')],
+            stars = [...document.querySelectorAll('#register-inputs > span')]
+        inputs.map(input => input.setAttribute('style', 'border-color: rgba(0, 0, 0, 0.185); padding-right: 15px;'))
+        stars.map(star => star.style.visibility = 'hidden')
         const filtered = inputs.filter(input => !input.value)
-        filtered.map(input => input.style.borderColor = 'red')
-    }  
+        filtered.map(input => {
+            input.setAttribute('style', 'border-color: red; padding-right: 40px;')
+            const index = inputs.findIndex(inputs => inputs.name === input.name)
+            stars[index].style.visibility = 'visible'
+        })
+    }
+
+    noSpaces = () => {
+        const alert = document.querySelector('.register-space'),
+            input = document.querySelectorAll('#register-inputs > input')[3]
+        alert.style.visibility = 'visible'
+        input.setAttribute('style', 'border-color: red; padding-right: 90px;')
+    }
+
+    clearSpace = () => {
+        const alert = document.querySelector('.register-space'),
+            input = document.querySelectorAll('#register-inputs > input')[3]
+        input.style.paddingRight = '15px'
+        alert.style.visibility = 'hidden'
+    }
+
+    nameExists = () => {
+        const alert = document.querySelector('.register-name'),
+            input = document.querySelectorAll('#register-inputs > input')[3]
+        alert.style.visibility = 'visible'
+        input.setAttribute('style', 'border-color: red; padding-right: 110px;')
+    }
+
+    clearName = () => {
+        const alert = document.querySelector('.register-name'),
+            input = document.querySelectorAll('#register-inputs > input')[3]
+        input.style.paddingRight = '15px'
+        alert.style.visibility = 'hidden'
+    }
+
+    invalidEmail = () => {
+        const alert = document.querySelector('.register-at'),
+            input = document.querySelectorAll('#register-inputs > input')[2]
+        alert.style.visibility = 'visible'
+        input.setAttribute('style', 'border-color: red; padding-right: 90px;')
+    }
+
+    clearEmail = () => {
+        const alert = document.querySelector('.register-at'),
+            input = document.querySelectorAll('#register-inputs > input')[2]
+        input.style.paddingRight = '15px'
+        alert.style.visibility = 'hidden'
+    }
+
+    emailExists = () => {
+        const alert = document.querySelector('.register-email'),
+            input = document.querySelectorAll('#register-inputs > input')[2]
+        alert.style.visibility = 'visible'
+        input.setAttribute('style', 'border-color: red; padding-right: 90px;')
+    }
+
+    clearEmailExists = () => {
+        const alert = document.querySelector('.register-email'),
+            input = document.querySelectorAll('#register-inputs > input')[2]
+        input.style.paddingRight = '15px'
+        alert.style.visibility = 'hidden'
+    }
 
     registerUser = () => {
         const { firstName, lastName, username, password, email } = this.state
-
         // Check to make sure no fields were left blank
         if (firstName === "" || lastName === "" || username === "" || password === "" || email === "") {
+            this.clearSpace()
+            this.clearEmail()
+            this.clearEmailExists()
+            this.clearName()
             this.highLightFields()
         } else {
+            this.highLightFields()
             if (username.split(' ').length === 1) {
                 if (email.indexOf('@') > -1) {
                     axios.post(`/api/registerUser/`, { firstName, lastName, username, password, email }).then(res => {
                         if (res.data === 'email') {
                             //If email is already in use, inform user that email has been sent to their existing account's email address.
-                            this.setState({ RegisterMsg: 'An account with that email exists. An email has been sent to that account with the proper login information.' }, () => alert('Email is taken.'))
+                            this.clearSpace()
+                            this.clearEmail()
+                            this.clearName()
+                            this.emailExists()
                         } else if (res.data === 'username') {
                             //If username exists already, inform user to create a new one.
-                            this.setState({ registerMsg: 'That username already exists. Please choose another one.' }, () => alert('Username already exists. Choose another.'))
+                            this.clearSpace()
+                            this.clearEmail()
+                            this.clearEmailExists()
+                            this.nameExists()
                         } else if (res.data === 'success') {
                             //If all above is false, then registration is successful.
                             this.setState({ registerMsg: `Successfully registered ${username}! Login to access your lobby.` }, () => {
@@ -65,16 +137,26 @@ class Register extends Component {
                             })
                         }
                     })
-                } else alert('Missing "@" symbol in email address.')
+                } else {
+                    this.clearSpace()
+                    this.clearEmailExists()
+                    this.clearName()
+                    this.invalidEmail()
+                }
 
             }
-            else alert('Usernames cannot contain spaces')
+            else {
+                this.clearEmail()
+                this.clearEmailExists()
+                this.clearName()
+                this.noSpaces()
+            }
         }
     }
 
     render() {
         return (
-            <div style={{ overflowX: 'hidden' }}>
+            <div>
                 <div className="login-container">
                     <div className='login-header'>
                         <span>VR<span className='lighttext'>ART GALLERY</span></span>
@@ -82,10 +164,19 @@ class Register extends Component {
                     <div className='login-content' id='register-inputs'>
                         <h1>Register</h1>
                         <input id='register-focus' placeholder="First Name" name="firstName" onChange={this.handleChange}></input>
+                        <span className='_1'>*</span>
                         <input placeholder="Last Name" name="lastName" onChange={this.handleChange}></input>
+                        <span className='_2'>*</span>
                         <input placeholder="Email Address" name="email" onChange={this.handleChange}></input>
+                        <span className='_3'>*</span>
+                        <h6 className='register-at'>Invalid Email</h6>
+                        <h6 className='register-email'>Email Taken</h6>
                         <input placeholder="Username" name="username" onChange={this.handleChange}></input>
+                        <span className='_4'>*</span>
+                        <h6 className='register-space'>No Spaces :)</h6>
+                        <h6 className='register-name'>Username Taken</h6>
                         <input placeholder="Password" type="password" name="password" onChange={this.handleChange}></input>
+                        <span className='_5'>*</span>
                         <div onClick={() => this.registerUser()} style={{ marginBottom: '20px' }}>Register</div>
                     </div>
                     <h1 style={{ marginBottom: '20px' }}>Already have an account? <span className='login-sign' onClick={() => this.props.history.push('/login')}>Log In.</span></h1>
