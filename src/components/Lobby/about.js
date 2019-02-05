e925dc2d4d76ac59f2745b22eda05672d061528a
jakerowla <jakenrowland@gmail.com>
2019-02-04 19:07:46 -0700

    About section is now mobile responsive

--- src/components/Lobby/about.js
+++ src/components/Lobby/about.js
@@ -5,13 +5,25 @@ import '../../styles/Components/about.css'
 export default function (props) {
     const handleDrop = num => {
         const drops = document.querySelectorAll('[data-drop]')
-        if (drops[num].classList.contains('drop-anim')) {
-            drops[num].classList.remove('drop-anim')
-            drops[num].classList.add('up-anim')
+        if (num === 3) {
+            if (drops[num].classList.contains('drop-anim2')) {
+                drops[num].classList.remove('drop-anim2')
+                drops[num].classList.add('up-anim2')
+            }
+            else {
+                drops[num].classList.remove('up-anim2')
+                drops[num].classList.add('drop-anim2')
+            }
         }
         else {
-            drops[num].classList.remove('up-anim')
-            drops[num].classList.add('drop-anim')
+            if (drops[num].classList.contains('drop-anim')) {
+                drops[num].classList.remove('drop-anim')
+                drops[num].classList.add('up-anim')
+            }
+            else {
+                drops[num].classList.remove('up-anim')
+                drops[num].classList.add('drop-anim')
+            }
         }
     }
     const musicCitations = [`Gymnopedie No. 1 Kevin MacLeod (incompetech.com)
@@ -38,7 +50,7 @@ export default function (props) {
                     <p>Make sure your cursor is directly over the vinyl player and left-click to play your song selection.</p>
                 </div>
                 <div className='about-question' onClick={() => handleDrop(2)}>
-                    Why will my music not play in my gallery while on mobile?
+                    Why is music not playing on mobile?
             <i className="fas fa-chevron-down about-down center"></i>
                 </div>
                 <div className='about-dropdown center' data-drop>
@@ -48,8 +60,12 @@ export default function (props) {
                     Who performed the songs available as musical presets?
             <i className="fas fa-chevron-down about-down center"></i>
                 </div>
-                <div className='about-dropdown center' data-drop>
-                    <p>All songs were performed by Kevin Macleod, and they are all licensed under a creative commons license.  You can find more about him and his music <a href="http://freemusicarchive.org/music/Kevin_MacLeod/" target="_blank" className='about-link'>here</a>.</p>
+                <div className='about-dropdown center-column' data-drop>
+                    <p>All songs were performed by Kevin Macleod, and they are all licensed under a creative commons license.  You can find more about him and his music <a href="http://freemusicarchive.org/music/Kevin_MacLeod/" target="_blank" className='about-link'>here</a>, and the proper citations for each song are as follows:</p>
+                    <p>• Gymnopedie No. 1 Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By <a href='http://creativecommons.org/licenses/by/3.0/' target='_blank' className='about-link'>Attribution 3.0 License</a>.</p>
+                    <p>• Impromptu in Quarter Comma Meantone Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By <a href='http://creativecommons.org/licenses/by/3.0/' target='_blank' className='about-link'>Attribution 3.0 License</a>.</p>
+                    <p>• On the Passing of Time Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By <a href='http://creativecommons.org/licenses/by/3.0/' target='_blank' className='about-link'>Attribution 3.0 License</a>.</p>
+                    <p>• Snow Drop Kevin MacLeod (incompetech.com) Licensed under Creative Commons: By <a href='http://creativecommons.org/licenses/by/3.0/' target='_blank' className='about-link'>Attribution 3.0 License</a>.</p>
                 </div>
                 <div className='about-question' onClick={() => handleDrop(4)}>
                     Who created this project?
