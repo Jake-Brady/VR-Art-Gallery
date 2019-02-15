3b3ed6d8ad7621938d736a595549f7ebd063d4e4
Brady Snuggs <dronebug360@gmail.com>
2019-02-15 12:54:07 -0700

    removed unnecessary techs, libraries, and files.

--- src/views/ArtGallery.js
+++ src/views/ArtGallery.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router-dom'
 import {connect} from 'react-redux'
 import {setImages} from '../ducks/reducer'
